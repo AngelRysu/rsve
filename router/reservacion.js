a2d09f430b1dae3d934aead89623269dc9f06153
Jesus Angel Quezada Camacho <jesusangelquezada@192.168.8.234>
2025-07-15 15:06:22 -0600

    Delete a reservation

--- router/reservacion.js
+++ router/reservacion.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const {registrar_reservacion, obtener_reservaciones, confirmar_reservacion, obtener_reservaciones_dia, validar_reservas, obtener_reservas } = require("../controllers/reservacion")
+const {registrar_reservacion, obtener_reservaciones, confirmar_reservacion, obtener_reservaciones_dia, validar_reservas, obtener_reservas, cancelar_reservacion } = require("../controllers/reservacion")
 
 const routerReservacion = Router();
 
@@ -9,6 +9,6 @@ routerReservacion.get('/dia/:dia/:sala', obtener_reservaciones_dia);
 routerReservacion.get('/confirmar/:code', confirmar_reservacion);
 routerReservacion.post('/validation/', validar_reservas);
 routerReservacion.post('/reservas/', obtener_reservas);
-
+routerReservacion.delete('/cancelar/:code', cancelar_reservacion);
 
 module.exports = routerReservacion;
